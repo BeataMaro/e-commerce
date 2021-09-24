8e0e901a13d038324547253b20cbe0ba82e1750d
BeataMaro <beatamaro@interia.pl>
2021-09-24 15:33:41 +0200

    ThemeProvider styled-components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import { Container } from "react-bootstrap";
-
+import theme from "./theme";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
@@ -9,18 +10,20 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <main className='py-3 justify-content-center'>
-        <Container>
-          <Switch>
-            <Route path='/' exact component={HomeScreen} />
-            <Route path='/product/:id' component={ProductScreen} />
-          </Switch>
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header />
+        <main className='py-3 justify-content-center'>
+          <Container>
+            <Switch>
+              <Route path='/' exact component={HomeScreen} />
+              <Route path='/product/:id' component={ProductScreen} />
+            </Switch>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   );
 };
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Card,
+  Button,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import products from "../products";
+
+const StyledListGroupItem = styled(ListGroupItem)`
+  background-color: white;
+  color: ${({ theme }) => theme.colors.main};
+`;
 
 const ProductScreen = ({ match }) => {
   const product = products.find((item) => item._id === match.params.id);
@@ -18,13 +32,45 @@ const ProductScreen = ({ match }) => {
         </Col>
         <Col md={3}>
           <ListGroup variant='flush'>
-            <ListGroup.Item>
+            <ListGroupItem variant='light'>
               <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating rate={product.rating} numReviews={product.numReviews} />
-            </ListGroup.Item>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                rate={product.rating}
+                numReviews={`${product.numReviews} ocen`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>{product.price}zł</ListGroupItem>
+            <ListGroupItem>Opis produktu: ${product.description}</ListGroupItem>
           </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <StyledListGroupItem>
+                <Row>
+                  <Col>Suma:</Col>
+                  <Col>{product.price}zł</Col>
+                </Row>
+              </StyledListGroupItem>
+              <StyledListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "Dostępny" : "Niedostępny"}
+                  </Col>
+                </Row>
+              </StyledListGroupItem>
+              <StyledListGroupItem>
+                <Row className='d-grid'>
+                  <Button type='button' disabled={product.countInStock === 0}>
+                    Dodaj do koszyka
+                  </Button>
+                </Row>
+              </StyledListGroupItem>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </>
