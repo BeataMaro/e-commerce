0d12d54852c6f24eac51adb1d786ce0956b9f693
BeataMaro <beatamaro@interia.pl>
2021-09-25 00:05:55 +0200

    express +  .env

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
+// import products from "../products";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      //res.data
+      const { data } = await axios.get("/api/products"); //proxy we frontend/package.json określa localhost na 8000 a nie 3000
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Col className='text-center'>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import {
@@ -11,7 +12,7 @@ import {
   Button,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+// import products from "../products";
 
 const StyledListGroupItem = styled(ListGroupItem)`
   background-color: white;
@@ -19,7 +20,18 @@ const StyledListGroupItem = styled(ListGroupItem)`
 `;
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((item) => item._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      //res.data
+      const { data } = await axios.get(`/api/products/${match.params.id}`); //proxy we frontend/package.json określa localhost na 8000 a nie 3000
+
+      // const product = await data.find((item) => item._id === match.params.id);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match]); //dependency
 
   return (
     <>
